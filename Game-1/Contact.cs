ca5069a4a66de6661de63cf6f4c814d526cfec78
Hurricane-unlegal <pavelsmith15@gmail.com>
2018-11-05 15:08:18 +0300

    Начало

---
+++ Game-1/Contact.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_1
+{
+    class Contact
+    {
+        //У варвара не отнимается здоровье
+        Random r = new Random();
+        public void Barbarian()
+        {
+            int HP = 100;
+            int DM = 5;
+            do
+            {
+            Console.Clear();
+            Console.WriteLine("Бой с варваром");
+            Console.WriteLine("Здоровье варвара "+ HP);
+            Defend(HP);
+            Console.Clear();
+            Console.WriteLine("Бой с варваром");
+            Console.WriteLine("Здоровье варвара " + HP);
+            Attack(DM);
+            } while (HP > 0);
+            Console.WriteLine("Варвар погиб");
+            Console.ReadKey();
+        }
+        void Defend(int HP)
+        {            
+            
+            Console.WriteLine("1 - ударить ногой");
+            Console.WriteLine("2 - ударить рукой");
+            int f = Convert.ToInt32(Console.ReadKey().KeyChar);
+            switch (f)
+            {
+                case 1: HP -= r.Next(20, 30);break;
+                case 2: HP -= r.Next(20, 30); break;
+            }
+        }
+        int Attack(int damage)
+        {
+            Console.WriteLine("1 - защитить голову");
+            Console.WriteLine("2 - защитить тело");
+            int v = r.Next(1, 2);
+            int d = Convert.ToInt32(Console.ReadKey().KeyChar);
+            if (v == d)
+            {
+                damage = r.Next(3, 7);
+            }
+            else
+            {
+                damage = r.Next(21, 31);
+            }
+            return damage;
+        }
+    }
+}
---
+++ Game-1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_1
+{
+    class Program
+    {
+        
+        static int frames = 0;
+        static int[,] map = new int [11,11];
+        static int x = 5;
+        static int y = 5;
+        static int floor = 1;
+        static void Main(string[] args)
+        {
+            int a = 0;
+            mapGen();
+            renderMap();
+            do
+            {
+                Input();
+            } while (a != 1);             
+            Console.WriteLine("This is the end");
+            Console.ReadKey();
+        }
+        static void mapGen()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                for(int j = 0; j < 11; j++)
+                {
+                    map[i, j] = r.Next(0, 4);
+                    if (i == 5 && j == 5)
+                    {
+                        map[i, j] = 99;
+                    }
+                    if (i == 0 || j == 0 || i == 10 || j == 10)
+                    {
+                        map[i, j] = 0;
+                    }
+                }
+            }
+        }
+        static void renderMap ()
+        {
+            frames++;
+            Console.Clear();
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {                   
+                    switch (map[i, j])
+                    {
+                        case 0: Console.Write('#'); break;
+                        case 1: Console.Write('.'); break;
+                        case 2: Console.Write('.'); break;
+                        case 3: Console.Write('*'); break;
+                        case 99: Console.Write('@'); break;
+                    }                    
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine(frames);
+        }
+        static void movement(int dx,int dy)
+        {
+            if (map[x + dx, y + dy] != 0)
+            {               
+                map[x, y] = floor;          
+                floor = map[x + dx, y + dy];
+                map[x + dx, y + dy] = 99;                
+                x += dx;y += dy;
+                renderMap();
+            }
+        }
+        static void Input()
+        {
+            char b;           
+            b = Convert.ToChar(Console.ReadKey().KeyChar);
+            Contact barbar = new Contact();
+            switch (b)
+            {
+                case '0': barbar.Barbarian(); break;
+                case '5': mapGen(); break;
+                case '8': movement(-1, 0); break;
+                case '2': movement(1, 0); break;
+                case '6': movement(0, 1); break;
+                case '4': movement(0, -1); break;
+                case '7': movement(-1, -1); break;
+                case '9': movement(-1, 1); break;
+                case '3': movement(1, 1); break;
+                case '1': movement(1, -1); break;
+            }            
+        }
+        static void Events()
+        {
+            Contact barbar = new Contact();
+            switch (floor)
+            {
+                case 3: barbar.Barbarian(); break;
+            }
+        }
+    }   
+}
